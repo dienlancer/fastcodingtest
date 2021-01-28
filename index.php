ef03070c6c7809276e1643cef055c5a505e2b57e
DESKTOP-QSMSQE0\diennk <trietnk01@gmail.com>
2021-01-28 16:58:43 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="https://chonviec.vn/upload/chonviec-favicon-red-5134680279.png" type=image/x-icon>
+    <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="demo-scss/style-dienkim.css" />
+</head>
+
+<body>
+    <div class="header-box">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="header-wrapper">
+                        <img src="images/logo.png" />
+                        <ul class="icons">
+                            <li><a href="javascript:void(0);"><i class="fa fa-phone" aria-hidden="true"></i></a></li>
+                            <li><a href="javascript:void(0);"><i class="fa fa-map-marker" aria-hidden="true"></i></a>
+                            </li>
+                            <li><a href="javascript:void(0);"><i class="fa fa-envelope-o" aria-hidden="true"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                            <li class="nav-item active">
+                                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">What about ISAMA</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">What we do ?</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Our projects</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Excelent team</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Contact</a>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <img src="images/banner.png" />
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="images/den-hoc.png" />
+                    </div>
+                    <div class="col-md-9">
+                        <h3>What about ISAMA</h3>
+                        <div class="rot">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="row">
+                    <div class="col-md-3">
+                        <img src="images/den-hoc.png" />
+                    </div>
+                    <div class="col-md-9">
+                        <h3>What about ISAMA</h3>
+                        <div class="rot">
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
+                            been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
+                            galley of type and scrambled it to make a type specimen book. It has survived not only five
+                            centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum
+                            passages, and more recently with desktop publishing software like Aldus PageMaker including
+                            versions of Lorem Ipsum
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="keo-cong">
+        <img src="images/keo-cong.png" />
+    </div>
+    <script src="bootstrap/js/bootstrap.min.js" defer></script>
+</body>
+
+</html>
